44f35742d40cb26b8b5f0b465514d71837ebd83e
Terence Lee <terencelee1234@gmail.com>
2020-02-27 11:56:52 +0800

    Update WheelCurvedPicker.android.js

--- WheelCurvedPicker.android.js
+++ WheelCurvedPicker.android.js
@@ -1,10 +1,8 @@
 'use strict';
 
 import React from 'react';
-import PropTypes from 'prop-types'
 import {
 	View,
-	ColorPropType,
 	requireNativeComponent,
 } from 'react-native';
 
@@ -12,42 +10,11 @@ const defaultItemStyle = { color: 'white', fontSize: 26 };
 
 const WheelCurvedPickerNativeInterface = {
 	name: 'WheelCurvedPicker',
-	propTypes: {
-		...View.propTypes,
-		data:PropTypes.array,
-		textColor: ColorPropType,
-		textSize: PropTypes.number,
-		itemStyle: PropTypes.object,
-		itemSpace: PropTypes.number,
-		onValueChange: PropTypes.func,
-		selectedValue: PropTypes.any,
-		selectedIndex: PropTypes.number,
-	}
 }
 
 const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker', WheelCurvedPickerNativeInterface);
 
 class WheelCurvedPicker extends React.Component {
-
-	propTypes: {
-		...View.propTypes,
-
-		data: PropTypes.array,
-
-		textColor: ColorPropType,
-
-		textSize: PropTypes.number,
-
-		itemStyle: PropTypes.object,
-
-		itemSpace: PropTypes.number,
-
-		onValueChange: PropTypes.func,
-
-		selectedValue: PropTypes.any,
-
-		selectedIndex: PropTypes.number,
-	}
 
 	constructor(props){
 		super(props)
@@ -97,10 +64,6 @@ class WheelCurvedPicker extends React.Component {
 }
 
 class Item extends React.Component {
-	propTypes: {
-		value: React.PropTypes.any, // string or integer basically
-		label: React.PropTypes.string,
-	}
 
 	render () {
 		// These items don't get rendered directly.
